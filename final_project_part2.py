da0e542f8b735928a1494a84986d09691a980328
Angela Fernando <angi.sf7@gmail.com>
2026-03-27 16:01:09 -0400

    added part 2 template

---
+++ final_project_part2.py
@@ -0,0 +1,12 @@
+import final_project_part1 as fp1
+
+# G = Directed weighted graph 
+# s = Source node 
+# d = Destination node 
+# h = Heuristic function. This is a dictionary which takes in a node (int)
+#     and returns a float. 
+def a_star(G, s, d, h): 
+    return  
+
+# return ({predecessor dictionary}, shortest path the algorithm determines
+# from s to d)
